1bb99ee0d232ae98589e7fb2577317221aaab166
Christopher Newton <christopher.k.newton@me.com>
2020-02-13 19:04:59 +1100

    Update intro text

--- ui/pages/home.js
+++ ui/pages/home.js
@@ -5,7 +5,7 @@ import projectListItem from '../components/project-list-item';
 
 export default ({ projects, posts }) => {
   const content = ragtag`
-  <p class="index-intro">I'm a Melbourne-based freelance <strong>UI Developer</strong> with a background in design. I've been building user interfaces and user experience prototypes for five years. These days I focus on web apps in <a target="_blank" href="https://reactjs.org">React</a> and <a target="_blank" href="https://nextjs.org">Next.js</a>.</p>
+  <p class="index-intro">I'm a Melbourne-based freelance <strong>UI Developer</strong> with a background in design. I've been building user interfaces and user experience prototypes for six years. These days I focus on web apps in <a target="_blank" href="https://reactjs.org">React</a> and <a target="_blank" href="https://nextjs.org">Next.js</a>.</p>
 
   ${projects && ragtag`
     <h3 class="subhead">Recent Projects</h3>
