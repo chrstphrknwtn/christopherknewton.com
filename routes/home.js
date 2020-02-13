ea32b73b0f433cbdc76e5694f9ed8163816fc54d
Christopher Newton <christopher.k.newton@me.com>
2020-02-13 19:05:32 +1100

    Permit previewing draft content via query string on home page

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,14 @@
-import contentful from '../lib/contentful';
+import contentful, { contentfulPreview } from '../lib/contentful';
 import home from '../ui/pages/home';
 
 export default async (req, res) => {
   let posts;
   let projects;
 
+  const contentfulClient = req.query.preview ? contentfulPreview : contentful;
+
   try {
-    const response = await contentful.getEntries();
+    const response = await contentfulClient.getEntries();
 
     if (response.items.length > 0) {
       projects = response.items.filter(entry => entry.sys.contentType.sys.id === 'project');
