7f536f3ffea8a7df9b7c486df80d38867a3c1eee
Christopher Newton <christopher.k.newton@me.com>
2020-03-07 21:08:21 +1100

    Find a suitable thumbnail for tumblr photos posts more reliably

--- ui/pages/tumblrs.js
+++ ui/pages/tumblrs.js
@@ -7,6 +7,25 @@ const blogDescriptors = {
   '0x001e': 'painting'
 };
 
+const tumblrPost = post => {
+  if (post.type !== 'photo') {
+    return;
+  }
+
+  const images = post.photos[0].alt_sizes;
+  const thumbnail = images.find(i => i.width >= 250 && i.width <= 500);
+
+  if (!thumbnail) {
+    return;
+  }
+
+  return ragtag`
+    <a target="_blank" href="${post.post_url}">
+      <img src="${thumbnail.url}" />
+    </a>
+  `;
+};
+
 export default ({ tumblrs }) => {
   const content = ragtag`
     <h1 class="subhead">Visual reference</h1>
@@ -16,11 +35,7 @@ export default ({ tumblrs }) => {
         <p>
           <a href="${tumblr.blog.url}">${tumblr.blog.name}</a> · <em>${blogDescriptors[tumblr.blog.name]}</em>
         </p>
-        ${tumblr.posts.map(post => ragtag`
-          <a target="_blank" href="${post.post_url}">
-            <img src="${post.photos[0].alt_sizes[4].url}" />
-          </a>
-        `)}
+        ${tumblr.posts.map(post => tumblrPost(post))}
         </section>
       `)}
     </section>
