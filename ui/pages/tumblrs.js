ff87b46acd3796f6e6d6abe22abd83357511595b
Christopher Newton <christopher.k.newton@me.com>
2020-03-13 09:59:32 +1100

    dynamic positioning during scroll for tumblr columns

--- ui/pages/tumblrs.js
+++ ui/pages/tumblrs.js
@@ -20,7 +20,7 @@ const tumblrPost = post => {
   }
 
   return ragtag`
-    <a target="_blank" href="${post.post_url}">
+    <a class="tumblr-post" target="_blank" href="${post.post_url}">
       <img src="${thumbnail.url}" />
     </a>
   `;
@@ -33,12 +33,53 @@ export default ({ tumblrs }) => {
       ${tumblrs.map(tumblr => ragtag`
         <section class="tumblr-column">
         <p>
-          <a href="${tumblr.blog.url}">${tumblr.blog.name}</a> · <em>${blogDescriptors[tumblr.blog.name]}</em>
+          <a target="_blank" href="${tumblr.blog.url}">${tumblr.blog.name}</a> · <em>${blogDescriptors[tumblr.blog.name]}</em>
         </p>
         ${tumblr.posts.map(post => tumblrPost(post))}
+        <a target="_blank" class="tumblr-column--more" href="${tumblr.blog.url}">${tumblr.blog.name}.com</a>
         </section>
       `)}
     </section>
+    <script>
+      const doc = document;
+
+      const html = doc.documentElement;
+      const body = doc.body;
+
+      const columnWrapper = doc.querySelector('.tumblr-columns-wrapper');
+      const columnNodes = doc.querySelectorAll('.tumblr-column');
+      const columns = Array.prototype.slice.call(columnNodes);
+
+      function getScrollUnitInterval() {
+        const scrollTop = (html.scrollTop || body.scrollTop);
+        const scrollHeight = (html.scrollHeight || body.scrollHeight);
+        const scroll = scrollTop / (scrollHeight - html.clientHeight);
+
+        if (scroll <= 0) {
+          return 0;
+        }
+        if (scroll >= 1) {
+          return 1;
+        }
+        return scroll;
+      }
+
+      function handler() {
+        const scroll = getScrollUnitInterval();
+        const columnMaxHeight = columnWrapper.clientHeight;
+
+        columns.forEach(col => {
+          const delta = columnMaxHeight - col.clientHeight;
+
+          if (delta > 0) {
+            col.style.transform = 'translateY(' + (delta * scroll) + 'px)';
+          }
+        });
+      }
+
+      doc.addEventListener('scroll', handler);
+      doc.addEventListener('resize', handler);
+    </script>
   `;
 
   return document({ content, title: 'Visual reference', hasWideContent: true });
