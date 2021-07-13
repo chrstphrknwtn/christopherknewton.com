a40eb279e97c061193626a49684a27be9df31296
Christopher Newton <christopher.k.newton@me.com>
2021-07-13 12:44:58 -0700

    Add RSS feeds

--- data/photographs.ts
+++ data/photographs.ts
@@ -1,4 +1,5 @@
 export type Photograph = {
+  publishDate: string;
   year: number;
   slug: string;
   title: string;
@@ -6,135 +7,127 @@ export type Photograph = {
 
 const photographs: Array<Photograph> = [
   {
+    publishDate: '2021-07-09',
     year: 2021,
     slug: '2021-lamp-shadow',
     title: 'Lamp Shadow'
   },
   {
+    publishDate: '2021-07-09',
     year: 2021,
     slug: '2021-lake-union',
     title: 'Lake Union, Seattle'
   },
   {
-    year: 2021,
-    slug: '2021-mopop-4',
-    title: 'MoPOP Museum 4'
-  },
-  /*
-  {
-    year: 2021,
-    slug: '2021-mopop-3',
-    title: 'MoPOP Museum 3'
-  },
-  {
+    publishDate: '2021-06-25',
     year: 2021,
     slug: '2021-mopop-2',
     title: 'MoPOP Museum 2'
   },
-  */
   {
+    publishDate: '2021-06-25',
     year: 2021,
     slug: '2021-mopop-1',
     title: 'MoPOP Museum 1'
   },
   {
+    publishDate: '2021-07-06',
     year: 2021,
     slug: '2021-ventura-ca-2',
     title: 'Ventura California 2'
   },
   {
+    publishDate: '2021-07-06',
     year: 2021,
     slug: '2021-ventura-ca-1',
     title: 'Ventura California 1'
   },
   {
+    publishDate: '2021-06-26',
     year: 2021,
     slug: '2021-ferry-sunset-2',
     title: 'Ferry Sunset 2'
   },
   {
+    publishDate: '2021-06-26',
     year: 2021,
     slug: '2021-ferry-sunset-1',
     title: 'Ferry Sunset 1'
   },
   {
+    publishDate: '2021-06-25',
     year: 2021,
     slug: '2021-northern-hemisphere-scene-1',
     title: 'Northern Hemisphere Scene 1'
   },
   {
+    publishDate: '2021-06-25',
     year: 2020,
     slug: '2020-offworld-alternate',
     title: 'Offworld Alternate'
   },
   {
+    publishDate: '2021-04-22',
     year: 2020,
     slug: '2020-offworld-1',
     title: 'Offworld 1'
   },
   {
+    publishDate: '2021-04-22',
     year: 2020,
     slug: '2020-offworld-2',
     title: 'Offworld 2'
   },
   {
+    publishDate: '2021-02-10',
     year: 2020,
     slug: '2020-main-street-usa-1',
     title: 'Forth Ave, Seattle'
   },
   {
+    publishDate: '2021-06-26',
     year: 2020,
     slug: '2020-eastern-wa-2',
     title: 'Eastern Washington 2'
   },
   {
+    publishDate: '2021-06-26',
     year: 2020,
     slug: '2020-eastern-wa-1',
     title: 'Eastern Washington 1'
   },
   {
+    publishDate: '2020-12-01',
     year: 2020,
     slug: '2020-fruit',
     title: 'Fruit'
   },
   {
+    publishDate: '2021-06-29',
     year: 2020,
     slug: '2020-parliament-and-palm-trees',
     title: 'Parliament and Palm Trees'
   },
   {
+    publishDate: '2020-12-01',
     year: 2020,
     slug: '2020-indexical-1',
     title: 'Indexical 1'
   },
   {
+    publishDate: '2020-12-01',
     year: 2020,
     slug: '2020-indexical-2',
     title: 'Indexical 2'
   },
   {
+    publishDate: '2020-12-01',
     year: 2019,
     slug: '2019-viewing-area',
     title: 'Viewing Area'
   },
-  /*
   {
-    year: 2019,
-    slug: '2019-solheimajokull',
-    title: 'Solheimajokull'
-  },
-  {
-    year: 2018,
-    slug: '2018-graduation-1',
-    title: 'Graduation 1'
-  },
-  {
-    year: 2018,
-    slug: '2018-graduation-2',
-    title: 'Graduation 2'
-  },
-  */
-  {
+    publishDate: '2020-12-01',
     year: 2010,
     slug: '2010-9600-meilleur',
     title: '9600 Meilleur'
---
+++ lib/rss.ts
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import { Feed } from 'feed';
+import c from 'copyright';
+
+import photographs, { Photograph } from '../data/photographs';
+
+const copyrightString = c({
+  name: 'Christopher Newton',
+  short: true,
+  startYear: 1981
+});
+
+(function () {
+  console.log('Building RSS feed');
+
+  if (process.env.NODE_ENV === 'development') {
+    return;
+  }
+
+  const baseUrl = 'https://christopherknewton.com';
+  const date = new Date();
+  const author = {
+    name: 'Christopher Newton',
+    email: 'hello@christopherknewton.com',
+    link: baseUrl
+  };
+
+  const feed = new Feed({
+    title: 'Christopher Newton',
+    description: 'Photography',
+    id: baseUrl,
+    link: baseUrl,
+    language: 'en',
+    // image: `${baseUrl}/images/logo.svg`,
+    // favicon: `${baseUrl}/favicon.ico`,
+    copyright: copyrightString,
+    updated: date,
+    feedLinks: {
+      rss2: `${baseUrl}/rss/feed.xml`,
+      json: `${baseUrl}/rss/feed.json`,
+      atom: `${baseUrl}/rss/atom.xml`
+    },
+    author
+  });
+
+  photographs.forEach((photograph: Photograph) => {
+    const url = `${baseUrl}/${photograph.slug}`;
+    feed.addItem({
+      title: `${photograph.title}, ${photograph.year}`,
+      description: `${photograph.title}, ${photograph.year}`,
+      content: `<figure>
+        <img src="/images/${photograph.slug}.jpg" alt="${photograph.title}" />
+      </figure>
+      <figcaption>${photograph.title}, ${photograph.year}.</figcaption>
+      `,
+      id: url,
+      link: url,
+      author: [author],
+      date: new Date(photograph.publishDate)
+    });
+  });
+
+  fs.mkdirSync('./public/rss', { recursive: true });
+  fs.writeFileSync('./public/rss/feed.xml', feed.rss2());
+  fs.writeFileSync('./public/rss/atom.xml', feed.atom1());
+  fs.writeFileSync('./public/rss/feed.json', feed.json1());
+})();
+
+export {};
