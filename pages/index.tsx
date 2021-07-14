ee808d4a9eeaa3268f8b5524c855258a8915dfd2
Christopher Newton <christopher.k.newton@me.com>
2021-07-14 15:26:22 -0700

    Use the latest/first image as the social image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import Head from 'next/head';
 import Layout from '../components/layout';
 import PhotographGrid from '../components/photograph-grid';
-import photographs from '../data/photographs';
+import photographs, { Photograph } from '../data/photographs';
 
-const AboutPage = () => (
+const AboutPage = ({
+  /* Use the latest photograph as the social image */
+  photograph = photographs[0]
+}: {
+  photograph: Photograph;
+}) => (
   <Layout isIndex>
+    <Head>
+      <title>Christopher Newton</title>
+      <meta name="description" content="Photographs" />
+      <meta name="og:title" content={`Christopher Newton`} />
+      <meta name="og:image" content={`/images/${photograph.slug}.jpg`} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
     <PhotographGrid photographs={photographs} />
   </Layout>
 );
